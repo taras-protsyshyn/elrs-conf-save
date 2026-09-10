ed965d1908c22148f75d11def61c4cb2e9947b6a
taras <protsyshyn.t@gmail.com>
2026-09-10 18:20:43 +0300

    add a base settings for run script in dev mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(
   (() => {
-    const app = document.createElement('div');
-    document.body.append(app);
+    const panel = document.getElementById("pane-justified-1").querySelectorAll(".mui-panel")[1];
+
+    const app = document.createElement("div");
+    panel.appendChild(app);
     return app;
   })(),
 ).render(
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
-import monkey from 'vite-plugin-monkey';
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import monkey from "vite-plugin-monkey";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     monkey({
-      entry: 'src/main.jsx',
+      entry: "src/main.jsx",
       userscript: {
-        icon: 'https://vitejs.dev/logo.svg',
-        namespace: 'npm/vite-plugin-monkey',
-        match: ['https://www.google.com/'],
+        icon: "https://vitejs.dev/logo.svg",
+        namespace: "npm/vite-plugin-monkey",
+        match: ["http://127.0.0.1:5500/index.html"],
       },
     }),
   ],
