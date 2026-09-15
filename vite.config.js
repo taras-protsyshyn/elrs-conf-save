7882e1a66deddde386a057f0654360b149f47a15
taras <protsyshyn.t@gmail.com>
2026-09-15 15:10:01 +0300

    update cofig

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       entry: "src/main.jsx",
       userscript: {
         namespace: "npm/vite-plugin-monkey",
-        match: ["http://127.0.0.1:5500"],
+        match: ["http://127.0.0.1:5500/", "http://127.0.0.1:5500/index.html"],
       },
     }),
   ],
